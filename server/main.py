dc1b0b0253d3f9f5029467413795c597d603398e
Aditya L <aditya.net09@gmail.com>
2022-12-05 18:26:33 -0800

    added post

--- server/main.py
+++ server/main.py
@@ -6,16 +6,18 @@ app = Flask(__name__)
 api = Api(app)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 # db = SQLAlchemy(app)
-login_args = reqparse.RequestParser()
-login_args.add_argument("username", type=str, help="username", required=True)
-login_args.add_argument("password", type=str, help="pass", required=True)
+# login_args = reqparse.RequestParser()
+# login_args.add_argument("username", type=str, help="username", required=True)
+# login_args.add_argument("password", type=str, help="pass", required=True)
 
 
-@app.route("/login", methods=["POST"])
+@app.route("/login", methods=[])
 def login():
-    if request.method == "POST":
-        args = login_args.parse_args()
-        # return success and role from db
+    # return success and role from db
+    username = request.form["username"]
+    password = request.form["password"]
+    print(username, password)
+    return "success"
 
 
 if __name__ == "__main__":
