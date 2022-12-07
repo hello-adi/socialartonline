1b1b5864fd1ed135404345249a6cfeda831dab05
Aditya L <aditya.net09@gmail.com>
2022-12-07 09:33:11 -0800

    API working, endpoints connected to db

--- sao-frontend/src/pages/CollapseForm.js
+++ sao-frontend/src/pages/CollapseForm.js
@@ -1,48 +1,55 @@
 import React, { useState } from "react";
-import useCollapse from 'react-collapsed';
+import useCollapse from "react-collapsed";
 
 function CollapseForm(props) {
+  const { getCollapseProps, getToggleProps } = useCollapse();
 
-    const { getCollapseProps, getToggleProps} = useCollapse();
+  const [temp, setTemp] = useState("");
 
-    const [temp, setTemp] = useState('');
+  const handleChange = (e) => {
+    setTemp(e.target.value);
+    e.preventDefault();
+  };
 
-    const handleChange = (e) => {
-        
-        setTemp(e.target.value);
-        e.preventDefault();
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let comment = temp;
+    console.log(props.source + "\n" + comment);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        let comment = temp;
-        console.log(props.source+'\n'+ comment);
-        //add comment to image using (props.source) and comment
-    }
-   
-    return (
-        <div className="collapsible">
-            <div className="header" {...getToggleProps()}>
-                <img src={props.source} alt="img1"/>
-            </div>
-            <div {...getCollapseProps()}>
-                <form onSubmit={handleSubmit}>
-                    <label>Rating:</label>
-                    <input type="range" min="1" max="5" list="steplist"/><br/>
-                    <datalist id="steplist">
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                    </datalist>
-                    <label>Post a Comment:</label><br/>
-                    <input type="text" placeholder='Comment' value={temp} onChange={handleChange}/><br/>
-                    <input type="submit" value="Post"/>
-                </form>
-            </div>
-        </div>
-    );
+    //add comment to image using (props.source) and comment
+  };
+
+  return (
+    <div className="collapsible">
+      <div className="header" {...getToggleProps()}>
+        <img src={props.source} alt="img1" />
+      </div>
+      <div {...getCollapseProps()}>
+        <form onSubmit={handleSubmit}>
+          <label>Rating:</label>
+          <input type="range" min="1" max="5" list="steplist" />
+          <br />
+          <datalist id="steplist">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </datalist>
+          <label>Post a Comment:</label>
+          <br />
+          <input
+            type="text"
+            placeholder="Comment"
+            value={temp}
+            onChange={handleChange}
+          />
+          <br />
+          <input type="submit" value="Post" />
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default CollapseForm;
--- sao-frontend/src/pages/home.js
+++ sao-frontend/src/pages/home.js
@@ -1,72 +1,95 @@
-import './home.css';
-import {React, Component} from 'react';
-import {useNavigate} from 'react-router-dom';
-import axios from 'axios';
+import "./home.css";
+import { React, Component } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 class Home extends Component {
-    constructor(props) {
-        
-        super(props);
-        this.state = {
-            username:'',
-            password:''
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+    };
 
-        this.handleUsername = this.handleUsername.bind(this);
-        this.handlePassword = this.handlePassword.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+    this.handleUsername = this.handleUsername.bind(this);
+    this.handlePassword = this.handlePassword.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    handleUsername(e) {
+  handleUsername(e) {
+    this.setState({ username: e.target.value });
+  }
 
-        this.setState({username: e.target.value});
-    }
+  handlePassword(e) {
+    this.setState({ password: e.target.value });
+  }
 
-    handlePassword(e) {
-        this.setState({password: e.target.value});
-    }
+  handleSubmit(e) {
+    e.preventDefault();
+    let access = "/";
 
-    handleSubmit(e) {
-        e.preventDefault();
-        let access = '/';
+    const form = new FormData();
+    form.append("u", this.state.username);
+    form.append("p", this.state.password);
+    console.log(form);
 
-        axios({
-            method: 'post',
-            url: 'http://127.0.0.1:5000/login',
-            data: {username: "hello1", password: "world1"}
-        }).then(function(res) {
-            console.log(res);
-        }).catch(function(e) {
-            throw(e);
-        });
-        
-        //if username matches password in users table, change access to corresponding role
-        //i.e. if username password matches with moderator access='/moderator' etc.
-        //if username does not have a match, alert("Login info incorrect");        
+    const options = {
+      method: "POST",
+      url: "http://localhost:5000/login",
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      data: form,
+    };
 
-        this.props.navigate(access);
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response);
+        access = response["data"];
+        console.log(access);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
 
-    }
+    //if username matches password in users table, change access to corresponding role
+    //i.e. if username password matches with moderator access='/moderator' etc.
+    //if username does not have a match, alert("Login info incorrect");
+    console.log(access);
+    this.props.navigate(access);
+  }
 
-    render() {
-    
+  render() {
     return (
-            <div className='content'>
-                <form onSubmit={(e)=>this.handleSubmit(e)}>
-                    <h1>Login</h1><br/>
-                    <input type="text" placeholder='Username' value={this.state.username} onChange={this.handleUsername}/><br/>
-                    <input type="text" placeholder='Password' value={this.state.password} onChange={this.handlePassword}/><br/>
-                    <input type='submit' value='Go!'/>
-                </form>
-            </div>
-        );
-    } 
+      <div className="content">
+        <form onSubmit={(e) => this.handleSubmit(e)}>
+          <h1>Login</h1>
+          <br />
+          <input
+            type="text"
+            placeholder="Username"
+            value={this.state.username}
+            onChange={this.handleUsername}
+          />
+          <br />
+          <input
+            type="text"
+            placeholder="Password"
+            value={this.state.password}
+            onChange={this.handlePassword}
+          />
+          <br />
+          <input type="submit" value="Go!" />
+        </form>
+      </div>
+    );
+  }
 }
 
 export function HomeWithRouter(props) {
-    const navigate = useNavigate();
-    return(<Home navigate={navigate}></Home>)
+  const navigate = useNavigate();
+  return <Home navigate={navigate}></Home>;
 }
-  
+
 export default Home;
-  
